8cbf52c0c51b9006981bd513e474e6b05b9619ad
sbmcetsoc <sbmcetsoc@gmail.com>
2026-02-04 15:12:21 +0530

    updated details

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT: number = Number(process.env.PORT) || 3000;
+const HOST: string = '0.0.0.0';
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, '../public')));
@@ -16,7 +17,7 @@ app.get('/backspace', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/backspace.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST,() => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
